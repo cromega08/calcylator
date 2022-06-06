97f193e320b0e209903dc015d785f491620828fd
cromega <cr.jrg08@gmail.com>
2022-06-06 10:15:46 -0500

    calcylator.py

--- app.py
+++ app.py
@@ -1,63 +1,55 @@
 import argparse as argp
 import re
+import pyperclip as pyc
 
 class app():
 
     def __init__(self) -> None:
 
         self.parser = argp.ArgumentParser(prog = "calcylator",
+                                            prefix_chars = "~",
                                             description = "Resolve arimethics polynomials",
                                             epilog = "Superbus et Hedonistic~")
         self.parser.add_argument("operation",
                                 metavar = "OPERATION",
                                 type = str,
                                 help = "the arimethic operation (Ex.: 1+2-3*4/(5**6))")
-        # self.parser.add_argument("-a",
-        #                         "--add",
-        #                         action = "store_true",
-        #                         help = "set that only gonna add 2 numbers")
-        # self.parser.add_argument("-s",
-        #                         "--subtraction",
-        #                         action = "store_true",
-        #                         help = "set that only gonna subtract 2 numbers")
-
-        # self.parser.add_argument("-m",
-        #                         "--multiplication",
-        #                         action = "store_true",
-        #                         help = "set that only gonna multiply 2 numbers")
-
-        # self.parser.add_argument("-d",
-        #                         "--division",
-        #                         action = "store_true",
-        #                         help = "set that only gonna divide 2 numbers")
-
-        # self.parser.add_argument("-p",
-        #                         "--power",
-        #                         action = "store_true",
-        #                         help = "set that only gonna power a number")
-
+        self.parser.add_argument("~a",
+                                "~~absolute",
+                                action = "store_true",
+                                help = "set the output as an absolute value")
+        self.parser.add_argument("~cp",
+                                "~~copy",
+                                action = "store_true",
+                                help = "set the output value to be copied into the paperclip")
     def exec(self) -> None:
 
         argument = self.parser.parse_args()
-        operation = self.refine(argument.operation)
-        print(eval(operation))
+        operation = self.refine(argument.operation, argument.absolute)
+        result = eval(operation)
+        if argument.copy: pyc.copy(result); pyc.paste()
+        print(result) 
 
-    def refine(self, argument) -> bool:
+    def refine(self, argument, absolute) -> str:
 
         operation = argument.translate(str.maketrans("{}[],", "()()."))
-
-        regex_sub = {
-            "^/|^\*|[a-zA-Z]|[^+*-/\.\^()\d]": "",
-            "\^" : "\*\*",
+                
+        regex_subs = {
+            "^/+|^\*+|[a-zA-Z]|[^+*-/%\.\^()\d]": "",
+            "%" : "/100",
+            "\^" : "**",
             "/{2,}" : "/",
             "\+{2,}" : "+",
             "-{2,}" : "-",
             "\*{3,}" : "**",
-            "\.{2,}" : "."
+            "\.{2,}" : ".",
+            "\.[0-9]\." : ""
         }
 
-        for subs in regex_sub.items(): operation = re.sub(subs[0], subs[1], operation)     
+        for subs in regex_subs.items(): operation = re.sub(subs[0], subs[1], operation)     
+
+        if re.match("^\d|^\.[0-9]|^\+[0-9]|^-[0-9]", operation) == None: return self.refine(operation)
+
+        if absolute: return f"abs({operation})"
 
         return operation
-
-app().exec()
